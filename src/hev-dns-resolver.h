e289637b01c4d4d9fa5e120ac6e8b7e57b7035ad
Heiher <r@hev.cc>
2013-12-06 10:04:37 +0800

    HevDNSResolver: Move HevDNSHeader structure to source file.

--- src/hev-dns-resolver.h
+++ src/hev-dns-resolver.h
@@ -13,25 +13,6 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-typedef struct _HevDNSHeader HevDNSHeader;
-
-struct _HevDNSHeader
-{
-	uint16_t id;
-	uint8_t rd : 1;
-	uint8_t tc : 1;
-	uint8_t aa : 1;
-	uint8_t opcode : 4;
-	uint8_t qr : 1;
-	uint8_t rcode : 4;
-	uint8_t z : 3;
-	uint8_t ra : 1;
-	uint16_t qdcount;
-	uint16_t ancount;
-	uint16_t nscount;
-	uint16_t arcount;
-} __attribute__ ((packed));
-
 int hev_dns_resolver_new (void);
 bool hev_dns_resolver_query (int resolver, const char *server, const char *domain);
 unsigned int hev_dns_resolver_query_finish (int resolver);
