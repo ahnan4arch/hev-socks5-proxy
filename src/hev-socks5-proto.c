7ee9d8e4cafa3fef20418ec673a26e90913d3eac
Heiher <r@hev.cc>
2014-11-23 13:50:18 +0800

    HevSocks5Proto: Init.

---
+++ src/hev-socks5-proto.c
@@ -0,0 +1,11 @@
+/*
+ ============================================================================
+ Name        : hev-socks5-proto.c
+ Author      : Heiher <r@hev.cc>
+ Copyright   : Copyright (c) 2014 everyone.
+ Description : Socks5 protocol
+ ============================================================================
+ */
+
+#include "hev-socks5-proto.h"
+
---
+++ src/hev-socks5-proto.h
@@ -0,0 +1,14 @@
+/*
+ ============================================================================
+ Name        : hev-socks5-proto.h
+ Author      : Heiher <r@hev.cc>
+ Copyright   : Copyright (c) 2014 everyone.
+ Description : Socks5 protocol
+ ============================================================================
+ */
+
+#ifndef __HEV_SOCKS5_PROTO_H__
+#define __HEV_SOCKS5_PROTO_H__
+
+#endif /* __HEV_SOCKS5_PROTO_H__ */
+
