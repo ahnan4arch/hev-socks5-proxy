181e45ba5d2d84cb91970cfbe04aa187c68a7f16
Heiher <r@hev.cc>
2014-11-23 14:49:33 +0800

    HevSocks5Proto: Pack and unpack.

--- src/hev-socks5-proto.c
+++ src/hev-socks5-proto.c
@@ -7,5 +7,252 @@
  ============================================================================
  */
 
+#include <unistd.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
 #include "hev-socks5-proto.h"
+
+typedef struct _HevSocks5ProtoAuthReq HevSocks5ProtoAuthReq;
+typedef struct _HevSocks5ProtoAuthRes HevSocks5ProtoAuthRes;
+typedef struct _HevSocks5ProtoReqHeader HevSocks5ProtoReqHeader;
+typedef struct _HevSocks5ProtoResHeader HevSocks5ProtoResHeader;
+
+struct _HevSocks5ProtoAuthReq
+{
+	uint8_t ver;
+	uint8_t method_count;
+	uint8_t methods[255];
+} __attribute__ ((packed));
+
+struct _HevSocks5ProtoAuthRes
+{
+	uint8_t ver;
+	uint8_t method;
+} __attribute__ ((packed));
+
+struct _HevSocks5ProtoReqHeader
+{
+	uint8_t ver;
+	uint8_t cmd;
+	uint8_t rsv;
+	uint8_t atype;
+} __attribute__ ((packed));
+
+struct _HevSocks5ProtoResHeader
+{
+	uint8_t ver;
+	uint8_t rep;
+	uint8_t rsv;
+	uint8_t atype;
+} __attribute__ ((packed));
+
+int
+hev_socks5_proto_auth_req_pack (void *buffer, uint8_t method_count,
+			uint8_t *methods)
+{
+	HevSocks5ProtoAuthReq *req = buffer;
+
+	req->ver = 5;
+	req->method_count = method_count;
+	memcpy (&req->methods, methods, method_count);
+
+	return sizeof (2 + method_count);
+}
+
+int
+hev_socks5_proto_auth_req_unpack (void *buffer, uint16_t size,
+			uint8_t *method_count, uint8_t **methods)
+{
+	HevSocks5ProtoAuthReq *req = buffer;
+
+	if (2 > size)
+	      return -1;
+	if ((2 + req->method_count) > size)
+	      return -2;
+
+	*method_count = req->method_count;
+	*methods = req->methods;
+
+	return 0;
+}
+
+int
+hev_socks5_proto_auth_res_pack (void *buffer, uint8_t method)
+{
+	HevSocks5ProtoAuthRes *res = buffer;
+
+	res->ver = 5;
+	res->method = method;
+
+	return sizeof (HevSocks5ProtoAuthRes);
+}
+
+int
+hev_socks5_proto_auth_res_unpack (void *buffer, uint16_t size,
+			uint8_t *method)
+{
+	HevSocks5ProtoAuthRes *res = buffer;
+
+	if (sizeof (HevSocks5ProtoAuthRes) > size)
+	      return -1;
+
+	*method = res->method;
+
+	return 0;
+}
+
+int
+hev_socks5_proto_req_pack (void *buffer, uint8_t cmd, uint8_t atype,
+			const char *addr, uint16_t port)
+{
+	HevSocks5ProtoReqHeader *hdr = buffer;
+	void *paddr = buffer + sizeof (HevSocks5ProtoReqHeader);
+	uint32_t *ipv4 = paddr;
+	uint8_t *domain_len = paddr;
+	uint8_t *domain = paddr + 1;
+	uint16_t *pport, addr_len = 0;
+
+	hdr->ver = 5;
+	hdr->cmd = cmd;
+	hdr->atype = atype;
+	switch (atype) {
+	case HEV_SOCKS5_PROTO_ATYPE_IPV4:
+		*ipv4 = inet_addr (addr);
+		pport = paddr + 4;
+		addr_len = 4;
+		break;
+	case HEV_SOCKS5_PROTO_ATYPE_DOMAIN:
+		*domain_len = strlen (addr);
+		memcpy (domain, addr, *domain_len);
+		pport = paddr + 1 + *domain_len;
+		addr_len = 1 + *domain_len;
+		break;
+	case HEV_SOCKS5_PROTO_ATYPE_IPV6:
+		/* FIXME */
+	default:
+		return -1;
+	}
+
+	*pport = htons (port);
+
+	return sizeof (HevSocks5ProtoReqHeader) + addr_len + 2;
+}
+
+int
+hev_socks5_proto_req_unpack (void *buffer, uint16_t size, uint8_t *cmd,
+			uint8_t *atype, const char **addr, uint16_t *port)
+{
+	HevSocks5ProtoReqHeader *hdr = buffer;
+	uint8_t *paddr = buffer + sizeof (HevSocks5ProtoReqHeader);
+	uint8_t *domain_len = paddr;
+	uint16_t *pport;
+
+	if (sizeof (HevSocks5ProtoReqHeader) > size)
+	      return -1;
+
+	*cmd = hdr->cmd;
+	*atype = hdr->atype;
+	switch (hdr->atype) {
+	case HEV_SOCKS5_PROTO_ATYPE_IPV4:
+		if ((sizeof (HevSocks5ProtoReqHeader) + 4 + 2) > size)
+		      return -2;
+		*addr = (char *) paddr;
+		pport = (uint16_t *) paddr + 4;
+		break;
+	case HEV_SOCKS5_PROTO_ATYPE_DOMAIN:
+		if ((sizeof (HevSocks5ProtoReqHeader) + *domain_len + 2) > size)
+		      return -2;
+		paddr[domain_len[0] + 1] = '\0';
+		*addr = (char *) (paddr + 1);
+		pport = (uint16_t *) (paddr + 1 + *domain_len);
+		break;
+	case HEV_SOCKS5_PROTO_ATYPE_IPV6:
+		/* FIXME */
+	default:
+		return -1;
+	}
+
+	*port = ntohs (*pport);
+
+	return 0;
+}
+
+int
+hev_socks5_proto_res_pack (void *buffer, uint8_t rep, uint8_t atype,
+			const char *addr, uint16_t port)
+{
+	HevSocks5ProtoResHeader *hdr = buffer;
+	void *paddr = buffer + sizeof (HevSocks5ProtoResHeader);
+	uint32_t *ipv4 = paddr;
+	uint8_t *domain_len = paddr;
+	uint8_t *domain = paddr + 1;
+	uint16_t *pport, addr_len = 0;
+
+	hdr->ver = 5;
+	hdr->rep = rep;
+	hdr->atype = atype;
+	switch (atype) {
+	case HEV_SOCKS5_PROTO_ATYPE_IPV4:
+		*ipv4 = inet_addr (addr);
+		pport = paddr + 4;
+		addr_len = 4;
+		break;
+	case HEV_SOCKS5_PROTO_ATYPE_DOMAIN:
+		*domain_len = strlen (addr);
+		memcpy (domain, addr, *domain_len);
+		pport = paddr + 1 + *domain_len;
+		addr_len = 1 + *domain_len;
+		break;
+	case HEV_SOCKS5_PROTO_ATYPE_IPV6:
+		/* FIXME */
+	default:
+		return -1;
+	}
+
+	*pport = htons (port);
+
+	return sizeof (HevSocks5ProtoResHeader) + addr_len + 2;
+}
+
+int
+hev_socks5_proto_res_unpack (void *buffer, uint16_t size, uint8_t *rep,
+			uint8_t *atype, const char **addr, uint16_t *port)
+{
+	HevSocks5ProtoResHeader *hdr = buffer;
+	uint8_t *paddr = buffer + sizeof (HevSocks5ProtoResHeader);
+	uint8_t *domain_len = paddr;
+	uint16_t *pport;
+
+	if (sizeof (HevSocks5ProtoResHeader) > size)
+	      return -1;
+
+	*rep = hdr->rep;
+	*atype = hdr->atype;
+	switch (hdr->atype) {
+	case HEV_SOCKS5_PROTO_ATYPE_IPV4:
+		if ((sizeof (HevSocks5ProtoResHeader) + 4 + 2) > size)
+		      return -2;
+		*addr = (char *) paddr;
+		pport = (uint16_t *) paddr + 4;
+		break;
+	case HEV_SOCKS5_PROTO_ATYPE_DOMAIN:
+		if ((sizeof (HevSocks5ProtoResHeader) + *domain_len + 2) > size)
+		      return -2;
+		paddr[domain_len[0] + 1] = '\0';
+		*addr = (char *) (paddr + 1);
+		pport = (uint16_t *) (paddr + 1 + *domain_len);
+		break;
+	case HEV_SOCKS5_PROTO_ATYPE_IPV6:
+		/* FIXME */
+	default:
+		return -1;
+	}
+
+	*port = ntohs (*pport);
+
+	return 0;
+}
 
--- src/hev-socks5-proto.h
+++ src/hev-socks5-proto.h
@@ -10,5 +10,52 @@
 #ifndef __HEV_SOCKS5_PROTO_H__
 #define __HEV_SOCKS5_PROTO_H__
 
+#include <stdint.h>
+
+typedef enum _HevSocks5ProtoMethods HevSocks5ProtoMethods;
+typedef enum _HevSocks5ProtoCommands HevSocks5ProtoCommands;
+typedef enum _HevSocks5ProtoAddrTypes HevSocks5ProtoAddrTypes;
+
+enum _HevSocks5ProtoMethods
+{
+	HEV_SOCKS5_PROTO_METHOD_NOAUTH = 0x00,
+	HEV_SOCKS5_PROTO_METHOD_GSSAPI = 0x01,
+	HEV_SOCKS5_PROTO_METHOD_USERPASS = 0x02,
+	HEV_SOCKS5_PROTO_METHOD_NOACCEPTABLE = 0xFF,
+};
+
+enum _HevSocks5ProtoCommands
+{
+	HEV_SOCKS5_PROTO_CMD_CONNECT = 0x01,
+	HEV_SOCKS5_PROTO_CMD_BIND = 0x02,
+	HEV_SOCKS5_PROTO_CMD_UDP = 0x03,
+};
+
+enum _HevSocks5ProtoAddrTypes
+{
+	HEV_SOCKS5_PROTO_ATYPE_IPV4 = 0x01,
+	HEV_SOCKS5_PROTO_ATYPE_DOMAIN = 0x03,
+	HEV_SOCKS5_PROTO_ATYPE_IPV6 = 0x04,
+};
+
+int hev_socks5_proto_auth_req_pack (void *buffer, uint8_t method_count,
+			uint8_t *methods);
+int hev_socks5_proto_auth_req_unpack (void *buffer, uint16_t size,
+			uint8_t *method_count, uint8_t **methods);
+
+int hev_socks5_proto_auth_res_pack (void *buffer, uint8_t method);
+int hev_socks5_proto_auth_res_unpack (void *buffer, uint16_t size,
+			uint8_t *method);
+
+int hev_socks5_proto_req_pack (void *buffer, uint8_t cmd, uint8_t atype,
+			const char *addr, uint16_t port);
+int hev_socks5_proto_req_unpack (void *buffer, uint16_t size, uint8_t *cmd,
+			uint8_t *atype, const char **addr, uint16_t *port);
+
+int hev_socks5_proto_res_pack (void *buffer, uint8_t rep, uint8_t atype,
+			const char *addr, uint16_t port);
+int hev_socks5_proto_res_unpack (void *buffer, uint16_t size, uint8_t *rep,
+			uint8_t *atype, const char **addr, uint16_t *port);
+
 #endif /* __HEV_SOCKS5_PROTO_H__ */
 
