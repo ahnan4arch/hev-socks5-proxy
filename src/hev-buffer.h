9ea582fc9c167ad579a66b79d876b8b8e7e66bf4
Heiher <r@hev.cc>
2014-11-23 15:05:47 +0800

    HevBuffer: Init.

---
+++ src/hev-buffer.h
@@ -0,0 +1,27 @@
+/*
+ ============================================================================
+ Name        : hev-buffer.h
+ Author      : Heiher <r@hev.cc>
+ Copyright   : Copyright (c) 2014 everyone.
+ Description : Buffer
+ ============================================================================
+ */
+
+#ifndef __HEV_BUFFER_H__
+#define __HEV_BUFFER_H__
+
+#include <stdint.h>
+
+#define HEV_BUFFER_DATA_SIZE	(2044)
+
+typedef struct _HevBuffer HevBuffer;
+
+struct _HevBuffer
+{
+	uint16_t offset;
+	uint16_t length;
+	uint8_t data[HEV_BUFFER_DATA_SIZE];
+};
+
+#endif /* __HEV_BUFFER_H__ */
+
